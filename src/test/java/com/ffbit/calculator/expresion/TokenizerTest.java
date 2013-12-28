fd05fa67507d145102ffce2498b5c5208f6b817d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-12-28 22:04:57 +0200

    Tokenizer red The 'it should tokenize single digit expression' unit test is red (does not compile).

---
+++ src/test/java/com/ffbit/calculator/expresion/TokenizerTest.java
@@ -0,0 +1,20 @@
+package com.ffbit.calculator.expresion;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+public class TokenizerTest {
+
+    @Test
+    public void itShouldTokenizeSingleDigitExpression() throws Exception {
+        String expression = "1";
+        Tokenizer tokenizer = new Tokenizer();
+        assertThat(tokenizer.tokenize(expression), allOf((Matcher) hasSize(1), hasItem("1")));
+    }
+
+}
