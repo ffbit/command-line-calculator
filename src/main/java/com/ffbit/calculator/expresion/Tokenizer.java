2d44367cfc2f52ab11daf4cf69c06e0bc131a93b
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-12-28 22:13:45 +0200

    Tokenizer The 'it should tokenize single digit expression' unit test is red (does not pass assertions).

---
+++ src/main/java/com/ffbit/calculator/expresion/Tokenizer.java
@@ -0,0 +1,11 @@
+package com.ffbit.calculator.expresion;
+
+import java.util.Collection;
+
+public class Tokenizer {
+
+    public Collection<String> tokenize(String expression) {
+        return null;
+    }
+
+}
