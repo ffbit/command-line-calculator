1aaeb3ec617e3714b87d2561ad6b86ff5a054755
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-12-28 22:29:18 +0200

    Tokenizer The 'it should tokenize simplest addition expression' unit test is red (does not pass assertions).

--- src/test/java/com/ffbit/calculator/expresion/TokenizerTest.java
+++ src/test/java/com/ffbit/calculator/expresion/TokenizerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
@@ -15,6 +16,15 @@ public class TokenizerTest {
         String expression = "1";
         Tokenizer tokenizer = new Tokenizer();
         assertThat(tokenizer.tokenize(expression), allOf((Matcher) hasSize(1), hasItem("1")));
+    }
+
+    @Test
+    public void itShouldParseSimplestAdditionExpression() throws Exception {
+        String expression = "1+2";
+        Tokenizer tokenizer = new Tokenizer();
+
+        assertThat(tokenizer.tokenize(expression),
+                allOf((Matcher) hasSize(3), hasItems("1", "+", "2")));
     }
 
 }
