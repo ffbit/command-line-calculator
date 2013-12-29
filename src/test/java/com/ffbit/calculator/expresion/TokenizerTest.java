9090a5c332b787b40bb83481754ff0d4c819315c
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-12-29 13:36:21 +0200

    Tokenizer yellow Made use of ordered collection assertions.

--- src/test/java/com/ffbit/calculator/expresion/TokenizerTest.java
+++ src/test/java/com/ffbit/calculator/expresion/TokenizerTest.java
@@ -3,9 +3,7 @@ package com.ffbit.calculator.expresion;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.assertThat;
 
 public class TokenizerTest {
@@ -21,48 +19,42 @@ public class TokenizerTest {
     public void itShouldTokenizeSingleDigitExpression() throws Exception {
         expression = "1";
 
-        assertThat(tokenizer.tokenize(expression), hasItem("1"));
-        assertThat(tokenizer.tokenize(expression), hasSize(1));
+        assertThat(tokenizer.tokenize(expression), contains("1"));
     }
 
     @Test
     public void itShouldParseSimplestAdditionExpression() throws Exception {
         expression = "1+2";
 
-        assertThat(tokenizer.tokenize(expression), hasItems("1", "+", "2"));
-        assertThat(tokenizer.tokenize(expression), hasSize(3));
+        assertThat(tokenizer.tokenize(expression), contains("1", "+", "2"));
     }
 
     @Test
     public void itShouldTokenizeSimplestSubtractionExpression() throws Exception {
         expression = "1-2";
 
-        assertThat(tokenizer.tokenize(expression), hasItems("1", "-", "2"));
-        assertThat(tokenizer.tokenize(expression), hasSize(3));
+        assertThat(tokenizer.tokenize(expression), contains("1", "-", "2"));
     }
 
     @Test
     public void itShouldTokenizeSimplestMultiplicationExpression() throws Exception {
         expression = "1*2";
 
-        assertThat(tokenizer.tokenize(expression), hasItems("1", "*", "2"));
-        assertThat(tokenizer.tokenize(expression), hasSize(3));
+        assertThat(tokenizer.tokenize(expression), contains("1", "*", "2"));
     }
 
     @Test
     public void itShouldTokenizeSimplestDivisionExpression() throws Exception {
         expression = "1/2";
 
-        assertThat(tokenizer.tokenize(expression), hasItems("1", "/", "2"));
-        assertThat(tokenizer.tokenize(expression), hasSize(3));
+        assertThat(tokenizer.tokenize(expression), contains("1", "/", "2"));
     }
 
     @Test
     public void itShouldIgnoreWightSpaces() throws Exception {
         expression = " 1  +   2 ";
 
-        assertThat(tokenizer.tokenize(expression), hasItems("1", "+", "2"));
-        assertThat(tokenizer.tokenize(expression), hasSize(3));
+        assertThat(tokenizer.tokenize(expression), contains("1", "+", "2"));
     }
 
     @Test(expected = TokenizerException.class)
