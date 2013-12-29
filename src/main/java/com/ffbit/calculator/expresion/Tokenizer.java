587016716a1e2edd5fd0aae35f3c8a99dd9f0dbd
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-12-29 13:28:00 +0200

    Tokenizer green The 'it should tokenize simplest division expression' unit test is green.

--- src/main/java/com/ffbit/calculator/expresion/Tokenizer.java
+++ src/main/java/com/ffbit/calculator/expresion/Tokenizer.java
@@ -17,7 +17,8 @@ public class Tokenizer {
             if (Character.isDigit(c)
                     || c == '+'
                     || c == '-'
-                    || c == '*') {
+                    || c == '*'
+                    || c == '/') {
                 tokens.add(String.valueOf(c));
             } else {
                 throw new TokenizerException();
