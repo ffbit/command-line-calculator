d4ec54387c1c103cd1effb766e11e78665c16a1d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-12-29 14:10:05 +0200

    Tokenizer yellow Reduced the number of if-s for arithmetical operations' symbols.

--- src/main/java/com/ffbit/calculator/expresion/Tokenizer.java
+++ src/main/java/com/ffbit/calculator/expresion/Tokenizer.java
@@ -1,9 +1,17 @@
 package com.ffbit.calculator.expresion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Tokenizer {
+    private Set<Character> operations;
+
+    public Tokenizer() {
+        operations = new HashSet<Character>(Arrays.asList('+', '-', '*', '/'));
+    }
 
     public List<String> tokenize(String expression) {
         char[] chars = expression.toCharArray();
@@ -14,11 +22,8 @@ public class Tokenizer {
                 continue;
             }
 
-            if (Character.isDigit(c)
-                    || c == '+'
-                    || c == '-'
-                    || c == '*'
-                    || c == '/') {
+            if (Character.isDigit(c) ||
+                    operations.contains(c)) {
                 tokens.add(String.valueOf(c));
             } else {
                 throw new TokenizerException();
