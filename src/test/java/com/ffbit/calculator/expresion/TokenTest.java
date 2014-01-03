0e179ac2887977f6d8f94133311ffa6f4376abff
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-03 23:11:36 +0200

    Token red The 'it should have lexeme and type' unit test is red (does not compile).

---
+++ src/test/java/com/ffbit/calculator/expresion/TokenTest.java
@@ -0,0 +1,18 @@
+package com.ffbit.calculator.expresion;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TokenTest {
+
+    @Test
+    public void itShouldHaveLexemeAndType() throws Exception {
+        Token token = new Token("1", TokenType.LITERAL);
+
+        assertThat(token.getLexeme(), is("1"));
+        assertThat(token.getType(), is(TokenType.LITERAL));
+    }
+
+}
