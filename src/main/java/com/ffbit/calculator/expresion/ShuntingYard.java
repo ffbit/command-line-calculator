bfafe3ea463014dd8977d024d26aa68b17540fb3
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-05 00:49:19 +0200

    Reverse Polish Notation - Shunting Yard green The 'it should shunt simplest subtraction tokens to reverse Polish notation' unit test is green.

--- src/main/java/com/ffbit/calculator/expresion/ShuntingYard.java
+++ src/main/java/com/ffbit/calculator/expresion/ShuntingYard.java
@@ -1,15 +1,29 @@
 package com.ffbit.calculator.expresion;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ShuntingYard {
 
-    public List<Token> toRpn(List<Token> tokens) {
-        return Arrays.asList(
-                new Token("1", TokenType.LITERAL),
-                new Token("2", TokenType.LITERAL),
-                new Token("+", TokenType.ADDITION));
+    public List<Token> toRpn(List<Token> input) {
+        List<Token> output = new ArrayList<>();
+        Deque<Token> operatorStack = new LinkedList<>();
+
+        for (Token token : input) {
+            if (token.getType() == TokenType.LITERAL) {
+                output.add(token);
+            } else {
+                operatorStack.add(token);
+            }
+        }
+
+        while (!operatorStack.isEmpty()) {
+            output.add(operatorStack.pop());
+        }
+
+        return output;
     }
 
 }
