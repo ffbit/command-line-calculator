6c4e882df5604fbb1e11a84addf78a3f2752aeac
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-05 00:20:20 +0200

    Reverse Polish Notation - Shunting Yard red The 'it should shunt simplest addition tokens to reverse Polish notation' unit test is red (assertion errors).

---
+++ src/main/java/com/ffbit/calculator/expresion/ShuntingYard.java
@@ -0,0 +1,11 @@
+package com.ffbit.calculator.expresion;
+
+import java.util.List;
+
+public class ShuntingYard {
+
+    public List<Token> toRpn(List<Token> tokens) {
+        return null;
+    }
+
+}
