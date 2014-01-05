eec264ff7066884ffc4105caba2f8a1b1404045b
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-05 20:59:05 +0200

    Reverse Polish Notation - Evaluator red The 'it should evaluate one to one' unit test is red.

--- src/test/java/com/ffbit/calculator/expresion/RpnEvaluatorTest.java
+++ src/test/java/com/ffbit/calculator/expresion/RpnEvaluatorTest.java
@@ -2,7 +2,9 @@ package com.ffbit.calculator.expresion;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -14,6 +16,14 @@ public class RpnEvaluatorTest {
         RpnEvaluator evaluator = new RpnEvaluator();
 
         assertThat(evaluator.evaluate(Collections.<Token>emptyList()), is(0));
+    }
+
+    @Test
+    public void itShouldEvaluateOneToOne() throws Exception {
+        RpnEvaluator evaluator = new RpnEvaluator();
+        List<Token> rpnTokens = Arrays.asList(new Token("1", TokenType.LITERAL));
+
+        assertThat(evaluator.evaluate(rpnTokens), is(1));
     }
 
 }
