68a7d28da387306d20d3e6b57eee74cc854fe77a
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-01-07 20:58:03 +0200

    CLI green The 'it should out one on one' unit test is green.

--- src/main/java/com/ffbit/calculator/cli/CalculatorMain.java
+++ src/main/java/com/ffbit/calculator/cli/CalculatorMain.java
@@ -1,9 +1,21 @@
 package com.ffbit.calculator.cli;
 
+import java.util.Scanner;
+
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        boolean wasInput = false;
+
+        while (scanner.hasNext()) {
+            wasInput = true;
+            System.out.println(scanner.next());
+        }
+
+        if (!wasInput) {
+            System.out.println();
+        }
     }
 
 }
